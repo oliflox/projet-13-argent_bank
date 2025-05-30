7ce580eaf7bd97788ff039a5da2a81b2cbec1846
oliflox <83953551+oliflox@users.noreply.github.com>
2025-05-30 15:30:03 +0200

    Remove NavConnected and NavGuest components; integrate Navigation component

---
+++ front-end/src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navigation({ isAuthenticated, firstName }) {
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src="./src/assets/img/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        {isAuthenticated ? (
+          <>
+            <Link className="main-nav-item" to="/profile">
+              <i className="fa fa-user-circle"></i>
+              {firstName}
+            </Link>
+            <Link className="main-nav-item" to="/sign-out">
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </>
+        ) : (
+          <Link className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation; 
